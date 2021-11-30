15b0cd27d492e41040bfa8a885a37506564e732a
Samantha <coderSam1088@gmail.com>
2021-11-30 12:42:21 -0500

    add X's

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    //initialize the state
+    super(props);
+    //use super to define the constructor of a subclass
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
